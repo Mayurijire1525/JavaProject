c87ff36d3b95701f1ca33ccc955e2c33e95f460e
Mayurijire1525 <Mayurijire441@gmail.com>
2024-06-09 18:12:36 +0530

    Employee Management System

--- EmployeeManagement/src/com/app/customexception/CustomException.java
+++
@@ -1,9 +0,0 @@
-package com.app.customexception;
-
-@SuppressWarnings("serial")
-public class CustomException extends RuntimeException{
-	public CustomException(String msg) {
-		super(msg);
-	}
-
-}
