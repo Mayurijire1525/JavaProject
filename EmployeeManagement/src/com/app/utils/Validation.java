00aa165d4ae7fba260ea9e1238fe775efbc53778
Mayurijire1525 <Mayurijire441@gmail.com>
2024-06-09 16:12:39 +0530

    Employee Management

---
+++ EmployeeManagement/src/com/app/utils/Validation.java
@@ -0,0 +1,14 @@
+package com.app.utils;
+
+import com.app.entities.Employee;
+
+public class Validation {
+
+	public static Employee inputValidation(String next, String next2, String next3, String next4, double nextDouble) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
